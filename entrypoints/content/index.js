ec09bd12e9fc4a41e5b44b33529e34d04a70215a
onlyfortesting <shirolbagas@gmail.com>
2025-03-01 13:31:24 +0700

    refactor

--- entrypoints/content/index.js
+++ entrypoints/content/index.js
@@ -23,132 +23,141 @@ let isHomeModified
 //----------------------------------------------------------------------------------
 // Main Content Script Code
 //----------------------------------------------------------------------------------
-function loadContentScript(url, ctx) {
+async function loadContentScript(url, ctx) {
   if (url.pathname === "/") {
     if (url.hash || isHomeModified) return
 
-    waitFor(() =>
+    let whatCanIHelpWith = await waitFor(() =>
       $(selectorifyClass("mb-7 hidden text-center @lg/thread:block"))
-    ).then((whatCanIHelpWith) => {
-      whatCanIHelpWith.after(HomePrompts({}))
+    )
 
-      isHomeModified = true
-    })
+    whatCanIHelpWith.after(HomePrompts({}))
+    isHomeModified = true
   } else {
     let chats
-    waitFor(() => (chats = $$("[data-message-id]")).length).then(() => {
-      //----------------------------------------------------------------------------------
-      // Chat: Helpers
-      //----------------------------------------------------------------------------------
-      function getChatId(chat) {
-        return chat.dataset.messageId
+    await waitFor(() => (chats = $$("[data-message-id]")).length)
+    //----------------------------------------------------------------------------------
+    // Chat: Helpers
+    //----------------------------------------------------------------------------------
+    function getChatId(chat) {
+      return chat.dataset.messageId
+    }
+
+    async function getChatMarkdown(chat) {
+      if (chat.matches('[data-message-author-role="assistant"]')) {
+        chat._copyButton.click()
+
+        await sleep(50)
+
+        let clipboard = await sendMessage("read-clipboard", null, "background")
+        return clipboard
+      } else {
+        // role="user"
+        console.log($(chat, ".whitespace-pre-wrap"))
+        return $(chat, ".whitespace-pre-wrap").textContent
       }
+    }
 
-      async function getChatMarkdown(chat) {
-        if (chat.matches('[data-message-author-role="assistant"]')) {
-          chat._copyButton.click()
+    async function showDiff(chat) {
+      const pendingStoreKey = PENDING_DIFF_STORE_PREFIX + getChatId(chat)
+      //----------------------------------------------------------------------------------
+      // # Load pending diff, if it doesn't exist then create a new diff
+      //----------------------------------------------------------------------------------
+      let pending = await storage.getItem(pendingStoreKey)
+      let changes =
+        (pending?.length && pending) ||
+        compare(
+          await getChatMarkdown(chats[chats.indexOf(chat) - 1]),
+          await getChatMarkdown(chat)
+        )
 
-          await sleep(50)
+      let comparisonDom = await createDiffHtml(changes, pendingStoreKey)
+      //----------------------------------------------------------------------------------
+      // # Replace chat text with diff
+      //----------------------------------------------------------------------------------
+      let markdownContainer = await waitFor(() => $(chat, ".markdown"))
+      markdownContainer.style.whiteSpace = "pre-wrap"
+      markdownContainer.textContent = ""
+      markdownContainer.append(...comparisonDom)
+      //----------------------------------------------------------------------------------
+      // # Click listener for diff items
+      //----------------------------------------------------------------------------------
+      // Remove unused field to save storage space
+      changes.forEach((c) => delete c.count)
 
-          let clipboard = await sendMessage(
-            "read-clipboard",
-            null,
-            "background"
-          )
-          return clipboard
-        } else {
-          // role="user"
-          console.log($(chat, ".whitespace-pre-wrap"))
-          return $(chat, ".whitespace-pre-wrap").textContent
-        }
-      }
+      let addremove = changes.filter((c) => c.value)
 
-      async function showDiff(chat) {
-        const pendingStoreKey = PENDING_DIFF_STORE_PREFIX + getChatId(chat)
-        //----------------------------------------------------------------------------------
-        // # Load pending diff, if it doesn't exist then create a new diff
-        //----------------------------------------------------------------------------------
-        let pending = await storage.getItem(pendingStoreKey)
-        let changes =
-          (pending?.length && pending) ||
-          compare(
-            await getChatMarkdown(chats[chats.indexOf(chat) - 1]),
-            await getChatMarkdown(chat)
-          )
+      const undoer = createUndoer({
+        onUpdate() {
+          chat._undoer(this)
+        },
+      })
 
-        let comparisonDom = await createDiffHtml(changes, pendingStoreKey)
-        //----------------------------------------------------------------------------------
-        // # Replace chat text with diff
-        //----------------------------------------------------------------------------------
-        let markdownContainer = await waitFor(() => $(chat, ".markdown"))
-        markdownContainer.style.whiteSpace = "pre-wrap"
-        markdownContainer.textContent = ""
-        markdownContainer.append(...comparisonDom)
-        //----------------------------------------------------------------------------------
-        // # Click listener for diff items
-        //----------------------------------------------------------------------------------
-        // Remove unused field to save storage space
-        changes.forEach((c) => delete c.count)
+      $(chat)
+        .off("pointerdown", chat._diffclick)
+        .on(
+          "pointerdown",
+          (chat._diffclick = (e) => {
+            let closest = e.target.closest(".added,.removed")
+            if (!closest) return
 
-        let addremove = changes.filter((c) => c.value)
+            e.preventDefault()
+            e.stopPropagation()
 
-        const undoer = createUndoer({
-          onUpdate() {
-            chat._undoer(this)
-          },
-        })
+            let reject
+            if (e.buttons === 2) {
+              reject = true
 
-        $(chat)
-          .off("pointerdown", chat._diffclick)
-          .on(
-            "pointerdown",
-            (chat._diffclick = (e) => {
-              let closest = e.target.closest(".added,.removed")
-              if (!closest) return
+              // Disable context menu when right clicking (one-time)
+              $(e.currentTarget).on("contextmenu", (x) => x.preventDefault(), {
+                once: true,
+              })
+            }
 
-              e.preventDefault()
-              e.stopPropagation()
+            let nearby = [closest.nextSibling, closest.previousSibling].find(
+              (c) => c?._data?.added || c?._data?.removed
+            )
 
-              let reject
-              if (e.buttons === 2) {
-                reject = true
+            const batch = undoer.batcher()
+            const closestEmptier = document.createTextNode("")
+            const closestReplacer = document.createTextNode(closest._data.value)
+            if (nearby) {
+              if (reject) return
 
-                // Disable context menu when right clicking (one-time)
-                $(e.currentTarget).on(
-                  "contextmenu",
-                  (x) => x.preventDefault(),
-                  {
-                    once: true,
-                  }
-                )
-              }
+              const nearbyEmptier = document.createTextNode("")
 
-              let nearby = [closest.nextSibling, closest.previousSibling].find(
-                (c) => c?._data?.added || c?._data?.removed
-              )
+              batch.push(() => {
+                nearby.replaceWith(nearbyEmptier)
+                closest.replaceWith(closestReplacer)
 
-              const batch = undoer.batcher()
-              const closestEmptier = document.createTextNode("")
-              const closestReplacer = document.createTextNode(
-                closest._data.value
-              )
-              if (nearby) {
-                if (reject) return
+                const addremoveClone = addremove.slice()
+                addremove.splice(addremove.indexOf(nearby._data), 1)
+                addremove.splice(addremove.indexOf(closest._data), 1, {
+                  value: closest._data.value,
+                })
 
-                const nearbyEmptier = document.createTextNode("")
+                return () => {
+                  nearbyEmptier.replaceWith(nearby)
+                  closestReplacer.replaceWith(closest)
 
+                  addremove.length = 0
+                  addremove.push(...addremoveClone)
+                }
+              })
+            } else {
+              if (
+                (closest._data.added && !reject) ||
+                (closest._data.removed && reject)
+              ) {
                 batch.push(() => {
-                  nearby.replaceWith(nearbyEmptier)
                   closest.replaceWith(closestReplacer)
 
                   const addremoveClone = addremove.slice()
-                  addremove.splice(addremove.indexOf(nearby._data), 1)
                   addremove.splice(addremove.indexOf(closest._data), 1, {
                     value: closest._data.value,
                   })
 
                   return () => {
-                    nearbyEmptier.replaceWith(nearby)
                     closestReplacer.replaceWith(closest)
 
                     addremove.length = 0
@@ -156,86 +165,65 @@ function loadContentScript(url, ctx) {
                   }
                 })
               } else {
-                if (
-                  (closest._data.added && !reject) ||
-                  (closest._data.removed && reject)
-                ) {
-                  batch.push(() => {
-                    closest.replaceWith(closestReplacer)
+                batch.push(() => {
+                  closest.replaceWith(closestEmptier)
 
-                    const addremoveClone = addremove.slice()
-                    addremove.splice(addremove.indexOf(closest._data), 1, {
-                      value: closest._data.value,
-                    })
+                  const addremoveClone = addremove.slice()
+                  addremove.splice(addremove.indexOf(closest._data), 1)
 
-                    return () => {
-                      closestReplacer.replaceWith(closest)
+                  return () => {
+                    closestEmptier.replaceWith(closest)
 
-                      addremove.length = 0
-                      addremove.push(...addremoveClone)
-                    }
-                  })
-                } else {
-                  batch.push(() => {
-                    closest.replaceWith(closestEmptier)
-
-                    const addremoveClone = addremove.slice()
-                    addremove.splice(addremove.indexOf(closest._data), 1)
-
-                    return () => {
-                      closestEmptier.replaceWith(closest)
-
-                      addremove.length = 0
-                      addremove.push(...addremoveClone)
-                    }
-                  })
-                }
+                    addremove.length = 0
+                    addremove.push(...addremoveClone)
+                  }
+                })
               }
+            }
 
-              const save = () => {
-                const isResolved = !addremove.some((c) => c.added || c.removed)
-                if (isResolved) storage.removeItem(pendingStoreKey)
-                else storage.setItem(pendingStoreKey, addremove.slice())
-              }
+            const save = () => {
+              const isResolved = !addremove.some((c) => c.added || c.removed)
+              if (isResolved) storage.removeItem(pendingStoreKey)
+              else storage.setItem(pendingStoreKey, addremove.slice())
+            }
 
-              batch.push(() => {
-                save()
-                return () => save()
-              })
-
-              batch.done()
+            batch.push(() => {
+              save()
+              return () => save()
             })
-          )
-      }
-      //----------------------------------------------------------------------------------
-      // Chat: Augmenting Chats
-      //----------------------------------------------------------------------------------
-      chats.forEach((chat, i) => {
-        if (!chat.matches('[data-message-author-role="assistant"]')) return
 
-        let parent = $(chat, "^.group\\/conversation-turn")
-
-        chat._copyButton = $(parent, '[data-testid="copy-turn-action-button"]')
-
-        let buttonGroup = chat._copyButton.parentNode.parentNode
-
-        buttonGroup.append(
-          h(DiffWithPrevButton, {
-            chat,
-            onClick: () => {
-              showDiff(chat)
-            },
+            batch.done()
           })
         )
+    }
+    //----------------------------------------------------------------------------------
+    // Chat: Augmenting Chats
+    //----------------------------------------------------------------------------------
+    chats.forEach((chat, i) => {
+      if (!chat.matches('[data-message-author-role="assistant"]')) return
 
-        storage
-          .getItem(PENDING_DIFF_STORE_PREFIX + getChatId(chat))
-          .then(async (c) => {
-            if (!c?.length) return
+      let parent = $(chat, "^.group\\/conversation-turn")
 
+      chat._copyButton = $(parent, '[data-testid="copy-turn-action-button"]')
+
+      let buttonGroup = chat._copyButton.parentNode.parentNode
+
+      buttonGroup.append(
+        h(DiffWithPrevButton, {
+          chat,
+          onClick: () => {
             showDiff(chat)
-          })
-      })
+          },
+        })
+      )
+
+      storage
+        .getItem(PENDING_DIFF_STORE_PREFIX + getChatId(chat))
+        .then(async (c) => {
+          if (!c?.length) return
+
+          showDiff(chat)
+        })
     })
     //----------------------------------------------------------------------------------
     // Tooltip: Helpers
